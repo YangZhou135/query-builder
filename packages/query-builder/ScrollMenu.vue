414b718079695b77ec9c6dfbe02a67f41797fe89
YangZhou135 <350394781@qq.com>
2021-08-31 18:05:22 +0800

    修改字段选择弹窗的样式：滚动条以及tab定位滚动到顶部

--- packages/query-builder/ScrollMenu.vue
+++ packages/query-builder/ScrollMenu.vue
@@ -195,6 +195,17 @@ import emptyBox from './empty-box/empty-box.vue'
 
 </script>
 <style scoped>
+  /* 滚动条的宽度 */
+  ::-webkit-scrollbar {
+    width: 6px;
+    height: 6px;
+  }
+
+  /* 滚动条的滑块 */
+  ::-webkit-scrollbar-thumb {
+    background-color: #a1a3a9;
+    border-radius: 3px;
+  }
   .sibarmenu {
     width: 150px;
     height: calc(100vh - 350px);
@@ -212,6 +223,10 @@ import emptyBox from './empty-box/empty-box.vue'
 
   .search-parent-cate {
     display: grid;
+  }
+
+  .search-parent-cate:last-child {
+    margin-bottom: 300px;
   }
 
   .search-parent-title {
